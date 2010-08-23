b61d8da814afb9e5b536d8f1a5138293431312f3
Gerhard Gossen <gerhard.gossen@googlemail.com>
2010-08-23 14:42:50 +0200

    Move setup into bin/annotator

--- lib/Annotator/Gtk.pm
+++ lib/Annotator/Gtk.pm
@@ -3,13 +3,4 @@
 use warnings;
 use strict;
 
-use Annotator::Schema;
-use Annotator::Gtk::View;
-use Annotator::Gtk::Controller;
-
-my $model = Annotator::Schema->connect( 'dbi:Pg:dbname=enron', 'enron', 'enron' );
-my $view = Annotator::Gtk::View->new( directories => $model->resultset('Document')->directories );
-
-my $controller = Annotator::Gtk::Controller->new( view => $view )->run;
-
 1;
