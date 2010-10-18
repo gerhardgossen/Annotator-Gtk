06b515695899d8340526a4e185b19a8ceee6a3cf
Gerhard Gossen <gerhard.gossen@googlemail.com>
2010-10-18 09:35:48 +0200

    Add SQL script to clean empty annotation sets

---
+++ etc/remove_empty_annotationsets.sql
@@ -0,0 +1,4 @@
+﻿DELETE FROM annotationset WHERE annotationset_id IN (SELECT set.annotationset_id
+FROM annotationset as set LEFT JOIN annotationtype as ty USING (annotationset_id)
+GROUP BY annotationset_id
+HAVING COUNT(ty.annotationset_id) = 0)
