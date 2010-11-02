37dc774f38d3668a01c4f092a5c53913bd4d1954
Gerhard Gossen <gerhard.gossen@googlemail.com>
2010-11-02 13:20:08 +0100

    Add SQL helper scripts

---
+++ etc/join_counts.sql
@@ -0,0 +1,4 @@
+﻿SELECT document.*, ann_cnt.cnt
+FROM document NATURAL JOIN (SELECT text_id, COUNT(annotation_id) as cnt
+FROM document NATURAL JOIN text LEFT JOIN annotation using(text_id)
+GROUP BY text_id) as ann_cnt
