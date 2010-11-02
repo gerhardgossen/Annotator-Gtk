6d82593a2750205806768a284ae31f90209713f0
Gerhard Gossen <gerhard.gossen@googlemail.com>
2010-11-02 15:21:41 +0100

    Update SQL for additional automated emails

--- etc/tag_wordsmith_mails.sql
+++
@@ -1,4 +0,0 @@
-﻿INSERT INTO annotation (text_id, annotationtype_id, start_pos, end_pos, value, creator_id)
-SELECT DISTINCT text_id, 12 AS annotationtype_id, -1 AS start_pos, -1 AS end_pos, '' AS value, 1 AS creator_id
-FROM document
-WHERE title LIKE 'A.Word.A.Day%'
